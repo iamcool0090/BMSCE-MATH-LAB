e22ab7e48349bb7b8c7f5494ac4135ad99a80bf8
Praful Chandra <71261134+iamcool0090@users.noreply.github.com>
2023-01-26 19:44:23 +0530

    Update 1_Catenary.py

--- 1_Catenary.py
+++ 1_Catenary.py
@@ -1,10 +1,11 @@
 from sympy import plot_implicit, symbols,Eq,cosh
-import math
+
 x, y, a = symbols('x y a')
-a = 2
+a = 4
 # Define the equation for the shape
 eq = Eq(y,a*cosh(x/a))
 
 # Plot the shape
 
-p = plot_implicit(eq , (x,-20,20), (y,-20,20))
+p = plot_implicit(eq , (x,-20,20), (y,-20,20),title='$y=acosh(x/a)$',line_color='r')
+#p=plot_implicit(Eq(LHS,RHS),(x,leftside,rightside),(y,up,down),title='title',color='color')
