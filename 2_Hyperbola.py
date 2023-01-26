e19b10cd778f82da40fbc4a7774867e3f38db764
Praful Chandra <71261134+iamcool0090@users.noreply.github.com>
2023-01-26 19:46:34 +0530

    Update 2_Hyperbola.py

--- 2_Hyperbola.py
+++ 2_Hyperbola.py
@@ -1,10 +1,12 @@
 from sympy import plot_implicit, symbols,Eq
 import math
-x, y, a = symbols('x y a')
+x, y, a, b = symbols('x y a b')
 a = 2
+b = 4
 # Define the equation for the shape
-eq = Eq((x**2)/(a**2),1 + (y**2)/(a**2))
+eq = Eq((x**2)/(a**2),1 + (y**2)/(b**2))
 
 # Plot the shape
 
-plot_implicit(eq)
+p = plot_implicit(eq , (x,-20,20), (y,-20,20),title='$x^2/a^2 - y^2/b^2 = 1$',line_color='r')
+#p=plot_implicit(Eq(LHS,RHS),(x,leftside,rightside),(y,up,down),title='title',color='color')
