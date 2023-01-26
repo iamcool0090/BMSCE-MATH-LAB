1970c1e9df7cbd4fa4173f0d47454c725917690b
Praful Chandra <71261134+iamcool0090@users.noreply.github.com>
2023-01-26 20:01:47 +0530

    Update 7_Cissiod.py

--- 7_Cissiod.py
+++ 7_Cissiod.py
@@ -2,8 +2,11 @@ from sympy import plot_implicit, symbols,Eq
 x, y, a = symbols('x y a')
 a = 4
 # Define the equation for the shape
-eq = Eq(a*(y**2),x**3 + (y**2)*x)
+eq = Eq(y**2*(a-x),x**3)
+
 
 # Plot the shape
+p = plot_implicit(eq , (x,-4,4), (y,-4,4),title='$y^2(a-x) = x^3$',line_color='r')
+#p=plot_implicit(Eq(LHS,RHS),(x,leftside,rightside),(y,up,down),title='title',color='color')
 
-plot_implicit(eq)
+
