a6ca1da5cd1395050cbd2a37b1293c45bd59b02c
Praful Chandra <71261134+iamcool0090@users.noreply.github.com>
2023-01-26 20:39:38 +0530

    Update 4_Ellipse.py

--- Polar equations/4_Ellipse.py
+++ Polar equations/4_Ellipse.py
@@ -8,5 +8,9 @@ b = 10
 x = sqrt(a*sin(theta)**2 + b*cos(theta)**2)
 r = a*b/x
 polar(theta,r,"r")
-title("Ellipse\n")
+title("Ellipse : $ab/(asin^2(θ) + bcos^2(θ))**1/2$\n")
+
+#dont know how to write fraction as power in pylab
+
+
 show()
