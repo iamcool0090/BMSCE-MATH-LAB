7ae001e7a26496ad349384e87d698f39c782d5b8
Praful Chandra <71261134+iamcool0090@users.noreply.github.com>
2023-01-26 20:16:28 +0530

    Update 3_Lower_half_of_circle.py

--- 3_Lower_half_of_circle.py
+++ 3_Lower_half_of_circle.py
@@ -7,6 +7,5 @@ eq = Eq(x**2 + 2*x,4 + 4*y-y**2)
 
 # Plot the shape
 
-p = plot_implicit(eq , (x,-4,4), (y,-4,4),title='$x^2 + 2x = 4 + 4y-y^2$',line_color='r')
+p = plot_implicit(eq , (x,-5,3), (y,-2,0),title='$x^2 + 2x = 4 + 4y-y^2$',line_color='r')
 #p=plot_implicit(Eq(LHS,RHS),(x,leftside,rightside),(y,up,down),title='title',color='color')
-
