61e9e54d5f6298da1eba82d9b086a5731102e562
Praful Chandra <71261134+iamcool0090@users.noreply.github.com>
2023-01-26 19:59:25 +0530

    Update 6_Strophoid.py

--- 6_Strophoid.py
+++ 6_Strophoid.py
@@ -9,6 +9,6 @@ eq = Eq(y**2*(a-x),x**2*(a+x))
 
 # Plot the shape
 
-p = plot_implicit(eq , (x,-20,20), (y,-20,20),title='$y^2(a-x) = x^2(a+x)$',line_color='r')
+p = plot_implicit(eq , (x,-4,4), (y,-4,4),title='$y^2(a-x) = x^2(a+x)$',line_color='r')
 #p=plot_implicit(Eq(LHS,RHS),(x,leftside,rightside),(y,up,down),title='title',color='color')
 
