bb5cd3c621967d6021f85ee9ce0d883e5c5c19df
Praful Chandra <71261134+iamcool0090@users.noreply.github.com>
2023-01-26 20:08:21 +0530

    Update 4_Circle.py

--- 4_Circle.py
+++ 4_Circle.py
@@ -4,6 +4,9 @@ a = 4
 # Define the equation for the shape
 eq = Eq(x**2 + y**2, a**2)
 
+
+#given a in the question not sure if a or a^2
+
 # Plot the shape
 p = plot_implicit(eq , (x,-4,4), (y,-4,4),title='$x^2 + y^2 = a^2$',line_color='r')
 #p=plot_implicit(Eq(LHS,RHS),(x,leftside,rightside),(y,up,down),title='title',color='color')
