bfa0d2f853ce35092d94d46f84408392b3f4dd4c
Praful Chandra <71261134+iamcool0090@users.noreply.github.com>
2023-01-26 19:50:07 +0530

    Update 4_Circle.py

--- 4_Circle.py
+++ 4_Circle.py
@@ -2,7 +2,9 @@ from sympy import plot_implicit, symbols,Eq
 x, y, a = symbols('x y a')
 a = 4
 # Define the equation for the shape
-eq = Eq(x**2 + y**2, a)
+eq = Eq(x**2 + y**2, a**2)
 
 # Plot the shape
-plot_implicit(eq)
+p = plot_implicit(eq , (x,-4,4), (y,-4,4),title='$x^2 + y^2 = a^2$',line_color='r')
+#p=plot_implicit(Eq(LHS,RHS),(x,leftside,rightside),(y,up,down),title='title',color='color')
+
