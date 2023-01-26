1e4d902645f13f971249eac6d6050f94e7ceede8
Praful Chandra <71261134+iamcool0090@users.noreply.github.com>
2023-01-26 20:52:34 +0530

    Create 1_Circle.py

---
+++ Parametric Equations/1_Circle.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def circle(a):
+  x = []
+  y = []
+
+
+  for i in np.linspace(-10,10,50):
+    x.append(a * np.cos(i))
+    y.append(a * np.sin(i))
+
+
+
+
+  plt.plot(x,y)
+  plt.grid()
+  plt.title("Circle : $x = acos(θ); y = asin(θ)$")
+  plt.show()
+
+circle(10)
